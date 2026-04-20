591455c6e9634d985e09e24a2b56b829bafc7f5b
Rohit Sarkar <rohitsarkar034@gmail.com>
2026-04-20 14:32:34 +0530

    add: builtin prefetch of wrap around indexes

--- src/lock_free_spsc_queue.cpp
+++ src/lock_free_spsc_queue.cpp
@@ -1,4 +1,5 @@
 #define _POSIX_C_SOURCE 200809L
+
 #include <atomic>
 #include <stdlib.h>
 #include <cassert>
@@ -11,20 +12,21 @@
 LockFreeSPSCQueue::LockFreeSPSCQueue(int capacity) 
     : capacity(capacity) {
     size_t required_bytes = capacity * sizeof(QueueOrder);
-    //posix_memalign(reinterpret_cast<void**>(&m_order), 64, 65536);
     int returned_result = posix_memalign(reinterpret_cast<void**>(&m_order), 64, required_bytes);   
     assert(m_order != nullptr && "Since memory allocation failed, can't create queue \n");
     assert(returned_result == 0 && "Memory allocation assertion failed, return non-0 number \n");
 }
 
 bool LockFreeSPSCQueue::push_order_into_queue(const QueueOrder& qOrder) {
-    uint local_tail = tail;
-    uint local_head_cached = head;
-    if (!((local_tail - local_head_cached) < capacity)) {
-        std::cout << "Queue appears to be full, reloading atomic head \n";
-        head = head.load(std::memory_order_acquire);
+    //uint local_tail = tail_cached;
+    //uint local_head = head_cached;
+    if (!((tail - head_cached) < capacity)) {
+        //std::cout << "Queue appears to be full, reloading atomic head \n";
+        head_cached = head.load(std::memory_order_acquire);
     }
-    if (queue_full()) { return false; }
+    if (tail - head_cached >= capacity) { return false; }
+    int next_prefetch_index = (tail + L) & (capacity - 1);
+    __builtin_prefetch(&m_order[next_prefetch_index], 1, 3);
     m_order[tail & (capacity-1)] = qOrder;
     tail.store(tail+1, std::memory_order_release);
     assert(!queue_empty() && "Queue is empty.. \n");
@@ -32,26 +34,31 @@ bool LockFreeSPSCQueue::push_order_into_queue(const QueueOrder& qOrder) {
 }
 
 bool LockFreeSPSCQueue::pop_order_from_queue() {
-    uint local_tail_cached = tail;
-    if (local_tail_cached == head) {
-        std::cout << "Queue appears to be empty, reloading the atomic tail \n";
+    //uint local_tail = tail_cached;
+    int next_prefetch_index = (head + L) & (capacity - 1);
+    __builtin_prefetch(&m_order[next_prefetch_index], 0, 3);
+    if (tail_cached == head) {
+        //std::cout << "Queue appears to be empty, reloading the atomic tail \n";
         tail = tail.load(std::memory_order_acquire);
     }
     if (queue_empty()) { return false; }
-    QueueOrder* frontOrder = &m_order[head & (capacity - 1)];
     head.store(head + 1, std::memory_order_release);
     return true;
 }
 
 int LockFreeSPSCQueue::get_front_order_id() {
     if (queue_empty()) {
-        std::cout << "Since queue is empty, can't get front order id \n";
+        //std::cout << "Since queue is empty, can't get front order id \n";
         return 0;
     }
+    int next_prefetch_index = (head + L) & (capacity - 1);
+    __builtin_prefetch(&m_order[next_prefetch_index], 0, 3);
     return m_order[head & (capacity-1)].order_id;
 }
 
 void LockFreeSPSCQueue::display_all_orders_in_queue() {
+    int next_prefetch_index = (head + L) & (capacity - 1);
+    __builtin_prefetch(&m_order[next_prefetch_index], 0, 0);
     std::cout << "Current orders in the queue are: \n";
     for(int i = head; i < tail; i++) {
         std::cout << "Order id: " << m_order[i & (capacity - 1)].order_id << " \n";
