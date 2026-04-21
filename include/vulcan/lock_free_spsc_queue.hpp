6666c81017c04a2acc9d27bda23663c4b921872f
Rohit Sarkar <rohitsarkar034@gmail.com>
2026-04-21 13:09:03 +0530

    add: new structs for metadata producer and consumer and QueueOrder instance array in class

--- include/vulcan/lock_free_spsc_queue.hpp
+++ include/vulcan/lock_free_spsc_queue.hpp
@@ -14,13 +14,25 @@ struct alignas(64) QueueOrder {
     uint32_t quantity;
 };
 
+struct alignas(64) Metadata_Producer {
+    std::atomic<uint64_t> tail = 0;
+    uint64_t head_cached;
+};
+
+struct alignas(64) Metadata_Consumer {
+    std::atomic<uint64_t> head = 0;
+    uint64_t tail_cached;
+};
+
 class LockFreeSPSCQueue {
     public:
     LockFreeSPSCQueue(int capacity);
-    ~LockFreeSPSCQueue();
+    ~LockFreeSPSCQueue() = default;
     LockFreeSPSCQueue(const LockFreeSPSCQueue&) = delete;
     LockFreeSPSCQueue& operator=(const LockFreeSPSCQueue&) = delete;
     bool push_order_into_queue(const QueueOrder& qOrder);
+    static LockFreeSPSCQueue* create(int capacity);
+    static void destroy(LockFreeSPSCQueue* ptr);
     bool pop_order_from_queue();
     bool queue_full();
     bool queue_empty();
@@ -29,12 +41,9 @@ class LockFreeSPSCQueue {
     int get_front_order_id();
 
     private:
-    alignas(std::hardware_destructive_interference_size) std::atomic<uint64_t> tail = 0;
-    uint64_t head_cached;
-    alignas(std::hardware_destructive_interference_size) std::atomic<uint64_t> head = 0;
-    uint64_t tail_cached;
-    QueueOrder* m_order = nullptr; //8 bytes consumption
-    std::byte* raw_buffer = nullptr; //8 bytes consumption
+    Metadata_Producer mProducer;
+    Metadata_Consumer mConsumer;
     int capacity;
     static constexpr int L = 4;
+    QueueOrder q[];
 };
