7f0191c2d38853b5197ba3df7f4eaeaf71360ec7
Rohit Sarkar <rohitsarkar034@gmail.com>
2026-05-11 11:41:11 +0530

    add: compiler intrinsics in create function to keep the path warm

--- include/vulcan/lock_free_spsc_queue.hpp
+++ include/vulcan/lock_free_spsc_queue.hpp
@@ -3,7 +3,11 @@
 #include <atomic>
 #include <cassert>
 #include <cstddef>
+#include <cstdint>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <emmintrin.h>
 #include <exception>
 #include <fstream>
 #include <string>
@@ -13,6 +17,8 @@
 #include <limits>
 #include <new>
 #include <stdatomic.h>
+#include <smmintrin.h>
+#include <xmmintrin.h>
 
 struct alignas(64) QueueOrder {
     uint64_t order_id;
@@ -28,12 +34,14 @@ class LockFreeSPSCQueue {
         std::atomic<size_t> tail;
         size_t head_cached;
         static constexpr size_t mask = capacity - 1;
+        char padding[64 - ( (sizeof(uint64_t) * 2) % 64 )];
     };
 
     struct alignas(64) Metadata_Consumer {
         std::atomic<size_t> head;
         size_t tail_cached;
         static constexpr size_t mask = capacity - 1;
+        char padding[64 - ( (sizeof(uint64_t) * 2) % 64 )];
     };
 
     static LockFreeSPSCQueue* allocate_huge_pages();
@@ -50,7 +58,7 @@ class LockFreeSPSCQueue {
     bool queue_empty();
     int get_queue_current_size();
     int get_front_order_id();
-    T* buffer();
+    __restrict T* buffer();
 
     private:
     Metadata_Producer mProducer;
@@ -58,7 +66,7 @@ class LockFreeSPSCQueue {
 };
 
 template<typename T, size_t capacity>
-T* LockFreeSPSCQueue<T, capacity>::buffer() {
+__restrict T* LockFreeSPSCQueue<T, capacity>::buffer() {
     return reinterpret_cast<T*>(reinterpret_cast<char*>(this) + 2 * 64);
 }
 
@@ -66,6 +74,8 @@ template<typename T, size_t capacity>
 LockFreeSPSCQueue<T, capacity>::LockFreeSPSCQueue() : mProducer{0, 0}, mConsumer{0, 0} {
     assert(sizeof(Metadata_Producer) == 64 && "Size of metadata producer should be 64 bytes \n");
     assert(sizeof(Metadata_Consumer) == 64 && "Size of metadata consumer should be 64 bytes \n");
+    //char* offset_ptr = (char*)obj + 128;
+    //assert();
     assert(reinterpret_cast<uintptr_t>(this)%64 == 0 && "Assertion failed: Non functional core.. \n");
     assert(mProducer.mask == mConsumer.mask && "Mask in both producer and consumer threads should be equal \n");
     assert(sizeof(QueueOrder) % 64 == 0 && "QueueOrders should be 64 bytes in size \n");
@@ -78,13 +88,13 @@ LockFreeSPSCQueue<T, capacity>* LockFreeSPSCQueue<T, capacity>::create() {
     std::ifstream proc_file(required_proc_file_path);
     if (!proc_file.is_open()) {
         std::cerr << "Could not open file from path: " << required_proc_file_path << " \n";
-        std::terminate();
+        std::exit(EXIT_FAILURE);
     }
     int current_huge_pages = proc_file.get() - '0';
     std::cout << "The huge pages value is: " << current_huge_pages << " \n";
     if (current_huge_pages != 1) {
         std::cerr << "Huge pages still not 1. Terminating the program \n";
-        std::terminate();
+        std::exit(EXIT_FAILURE);
     }
     proc_file.close();
     const size_t boundary = 2 * 1024 * 1024;
@@ -93,9 +103,19 @@ LockFreeSPSCQueue<T, capacity>* LockFreeSPSCQueue<T, capacity>::create() {
     auto* mem_ptr = mmap(NULL, rounded_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_POPULATE, -1, 0);
     if (mem_ptr == MAP_FAILED) {
         perror("mmap");
-        std::terminate();
+        std::exit(EXIT_FAILURE);
     }
     LockFreeSPSCQueue* obj = new (mem_ptr) LockFreeSPSCQueue();
+    char* offset_ptr = (char*)obj + 128;
+    size_t L1_stride = 2048;
+    assert(((uintptr_t)offset_ptr % L1_stride) != 0 && "Buffer offset should not be a multiple of L1 critical stride \n");
+    __m128i* simd_obj_ptr = reinterpret_cast<__m128i*>(&mem_ptr);
+    //__m128i* simd_obj_ptr = reinterpret_cast<__m128i*>(&obj);
+    __m128i zero_vector = _mm_setzero_si128();
+
+    for(size_t i = 0; i < 2*1024*1024; i += 16) {
+        _mm_stream_si128(simd_obj_ptr + i, zero_vector);
+    }
     return obj;
 }
 
@@ -130,7 +150,6 @@ bool LockFreeSPSCQueue<T, capacity>::pop_order_from_queue() {
         mConsumer.tail_cached = mProducer.tail.load(std::memory_order_acquire);
         if (head == mConsumer.tail_cached) { return false; }
     }
-    //size_t slot_index = head & (capacity - 1); - popped element
     mConsumer.head.store((head+1)&(capacity-1), std::memory_order_release);
     return true;
 }
