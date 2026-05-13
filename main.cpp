c51a93d1a0c7feb3ea0d57ddf5f61d396c457914
Rohit Sarkar <rohitsarkar034@gmail.com>
2026-05-13 13:02:03 +0530

    fix: pop function return application inside main

--- main.cpp
+++ main.cpp
@@ -11,7 +11,10 @@ int main() {
     std::cout << "The emptyness state of the queue is: " << my_queue->queue_empty() << " \n";
     my_queue->push_order_into_queue({100, 22.32, 232});
     my_queue->push_order_into_queue({101, 23, 49});
+    assert(my_queue->get_queue_current_size() == 2 && "Queue size assertion failed, it should be 2 \n");
     std::cout << "The not emptyness state of the queue is: " << my_queue->queue_empty() << " \n";
-    my_queue->pop_order_from_queue();
+    const QueueOrder* popped_order = my_queue->pop_order_from_queue();
+    std::cout << "The popped order id is: " << popped_order->order_id << " \n";
+    assert(my_queue->get_queue_current_size() == 1 && "Queue size assertion failing \n");
     return 0;
 }
