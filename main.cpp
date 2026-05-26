e3b21e3a5ecd2135774302a1848c64d86089e8e2
Rohit Sarkar <rohitsarkar034@gmail.com>
2026-05-26 12:06:20 +0530

    add: function to populate stack buffer and compiler intrinsics in thread constructor loops to notify LSU that the thread is in a spin-wait

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #define _GNU_SOURCE
 
 #include <chrono>
@@ -17,6 +18,7 @@
 #include <sched.h>
 #include <type_traits>
 #include <thread>
+#include <cpuid.h>
 
 #include "vulcan/lock_free_spsc_queue.hpp"
 
@@ -31,7 +33,17 @@ void pinThreadToCore(pthread_t thread_id, int core_id) {
     }
 }
 
+bool populate_stack_buffer(std::array<QueueOrder, 1024>& my_queueOrder) { 
+    for (size_t i = 0; i < 1024; ++i) {
+        my_queueOrder[i] = {i, (i+1)*1.33, i+100};
+    }
+    assert(my_queueOrder[1023].order_id == 1023 && "Assertion failed: Stack buffer not populated correctly \n");
+    return true;
+}
+
 void push_orders_to_queue(LockFreeSPSCQueue<QueueOrder, 256>& queue, std::atomic<bool>& m_start_ref, int core_id) {
+    alignas(64) std::array<QueueOrder, 1024> queue_orders;
+    populate_stack_buffer(queue_orders);
     pthread_t current_thread_id = pthread_self();
     pinThreadToCore(current_thread_id, core_id);
     assert(sched_getcpu() == core_id && "Assertion failed: Pinning is not working \n");
@@ -40,11 +52,26 @@ void push_orders_to_queue(LockFreeSPSCQueue<QueueOrder, 256>& queue, std::atomic
         _mm_pause(); // notify LSU that the thread is in a spin-wait, reducing power consumption and preventing pipeline from being corrupted with speculative loads (evicting hot matching engine code from the L1i)
     }
     
-    const int TOTAL_CYCLES = 1000000;
-    QueueOrder dummy_instance = {100, 27.32, 102};
-    for (size_t i = 0; i < 100000; ++i) {
-        
+    size_t local_current_tail = 0;
+    size_t local_current_head_cached = 0;
+
+    unsigned int eax, ebx, ecx, edx;
+    __cpuid(0, eax, ebx, ecx, edx);
+    
+    unsigned long long start_time = __rdtsc();
+    
+    const int TOTAL_BURST_ORDERS = 100000000;
+    for (size_t i = 0; i < TOTAL_BURST_ORDERS; ++i) {
+        size_t next_loaal_tail = (local_current_tail + 1) & 255;
+        if (next_loaal_tail == local_current_head_cached) {
+            local_current_head_cached = queue.load_head_acquire();
+        }
+        size_t buffer_index = local_current_tail & 1023;
+        queue.push_order_into_queue(queue_orders[buffer_index]);
+        local_current_tail = (local_current_tail + 1) & 255;
     }
+    
+    unsigned long long end_time = __rdtsc();
     std::cout << "Producer thread is warmed up for now.. \n";
     return;
 }
@@ -53,13 +80,25 @@ void pop_orders_from_queue(LockFreeSPSCQueue<QueueOrder, 256>& queue, std::atomi
     pthread_t current_thread_id = pthread_self();
     pinThreadToCore(current_thread_id, core_id);
     assert(sched_getcpu() == core_id && "Assertion failed: Pinning is not working in consumer thread \n");
+    
+    size_t local_head_index = 0;
+    size_t local_tail_cached_test = 0;
 
     while (!m_start_ref.load(std::memory_order_acquire)) {
         _mm_pause(); //same as in push_orders_to_queue function (waiting for thread to finish OS-level init, pinning to cores, already spinning and hot in their L1i)
     }
+    const int TOTAL_BURST_ORDERS = 100000000;
+    for (size_t i = 0; i < TOTAL_BURST_ORDERS; ++i) {
+        auto* ptr = queue.peek_into_queue(local_head_index);
+        if (ptr == nullptr) {
+            local_tail_cached_test = queue.load_tail_acquire();
+            _mm_pause();
+        }
+        queue.commit_pop_order_from_queue(ptr, local_head_index);
+    }
     std::cout << "Consumer thread is warmed up right now.. \n";
     return;
-}
+}   
 
 int main() {
     int core1 = 0;
