dd9d55c92f182a30b2da72396842929f0d7094c4
Rohit Sarkar <rohitsarkar034@gmail.com>
2026-08-05 06:16:55 +0530

    add: formatting

--- include/vulcan/lock_free_spsc_queue.hpp
+++ include/vulcan/lock_free_spsc_queue.hpp
@@ -7,215 +7,244 @@
 #include <cstring>
 #include <emmintrin.h>
 #include <fstream>
-#include <string>
-#include <sys/mman.h>
 #include <iostream>
 #include <new>
+#include <numaif.h>
 #include <smmintrin.h>
+#include <string>
+#include <sys/mman.h>
 #include <type_traits>
 #include <xmmintrin.h>
-#include <numaif.h>
 
 struct alignas(64) QueueOrder {
-    uint64_t order_id;
-    double price;
-    uint32_t quantity;
+  uint64_t order_id;
+  double price;
+  uint32_t quantity;
+};
+
+template <typename T, size_t capacity> class LockFreeSPSCQueue {
+  static_assert((capacity & (capacity - 1)) == 0,
+                "Capacity must be a power of two");
+
+public:
+  struct alignas(std::hardware_destructive_interference_size)
+      Metadata_Producer {
+    std::atomic<size_t> tail;
+    size_t head_cached;
+    static constexpr size_t mask = capacity - 1;
+  };
+
+  struct alignas(std::hardware_destructive_interference_size)
+      Metadata_Consumer {
+    std::atomic<size_t> head;
+    size_t tail_cached;
+    static constexpr size_t mask = capacity - 1;
+  };
+
+  static LockFreeSPSCQueue *allocate_huge_pages();
+  LockFreeSPSCQueue();
+  ~LockFreeSPSCQueue() = default;
+  LockFreeSPSCQueue(const LockFreeSPSCQueue &) = delete;
+  LockFreeSPSCQueue &operator=(const LockFreeSPSCQueue &) = delete;
+  LockFreeSPSCQueue(LockFreeSPSCQueue &&) = delete;
+  LockFreeSPSCQueue &operator=(LockFreeSPSCQueue &&other) noexcept = delete;
+  inline size_t load_head_acquire() noexcept;
+  inline size_t load_tail_acquire() noexcept;
+  void producer_uncommitted_push(QueueOrder &qOrder,
+                                 size_t current_local_tail) noexcept;
+  const QueueOrder *
+  consumer_uncommitted_peek(size_t consumer_local_head) noexcept;
+  inline void publish_tail_release(size_t new_local_tail);
+  inline void publish_head_release(size_t new_local_head);
+  static LockFreeSPSCQueue *create();
+  static void destroy(LockFreeSPSCQueue *ptr);
+  void commit_pop_order_from_queue(const T *ptr, size_t index);
+  bool queue_full();
+  bool queue_empty();
+  int get_queue_current_size();
+  int get_front_order_id();
+  __restrict const T *buffer() noexcept;
+  void destroy_mapping() {
+    size_t size_required = sizeof(LockFreeSPSCQueue<T, capacity>);
+    this->~LockFreeSPSCQueue<T, capacity>();
+    munmap(this, size_required);
+  }
+
+private:
+  Metadata_Producer mProducer;
+  Metadata_Consumer mConsumer;
 };
 
 template <typename T, size_t capacity>
-class LockFreeSPSCQueue {
-    static_assert((capacity & (capacity - 1)) == 0, "Capacity must be a power of two");
-    public:
-    struct alignas(std::hardware_destructive_interference_size) Metadata_Producer {
-        std::atomic<size_t> tail;
-        size_t head_cached;
-        static constexpr size_t mask = capacity - 1;
-    };
-
-    struct alignas(std::hardware_destructive_interference_size) Metadata_Consumer {
-        std::atomic<size_t> head;
-        size_t tail_cached;
-        static constexpr size_t mask = capacity - 1;
-    };
-
-    static LockFreeSPSCQueue* allocate_huge_pages();
-    LockFreeSPSCQueue();
-    ~LockFreeSPSCQueue() = default;
-    LockFreeSPSCQueue(const LockFreeSPSCQueue&) = delete;
-    LockFreeSPSCQueue& operator=(const LockFreeSPSCQueue&) = delete;
-    LockFreeSPSCQueue(LockFreeSPSCQueue&&) = delete;
-    LockFreeSPSCQueue& operator=(LockFreeSPSCQueue&& other) noexcept = delete;
-    inline size_t load_head_acquire() noexcept;
-    inline size_t load_tail_acquire() noexcept;
-    void producer_uncommitted_push(QueueOrder& qOrder, size_t current_local_tail) noexcept;
-    const QueueOrder* consumer_uncommitted_peek(size_t consumer_local_head) noexcept;
-    inline void publish_tail_release(size_t new_local_tail);
-    inline void publish_head_release(size_t new_local_head);
-    //bool push_order_into_queue(const QueueOrder& qOrder);
-    static LockFreeSPSCQueue* create();
-    static void destroy(LockFreeSPSCQueue* ptr);
-    //const T* peek_into_queue(size_t& current_index) noexcept;
-    void commit_pop_order_from_queue(const T* ptr, size_t index);
-    bool queue_full();
-    bool queue_empty();
-    int get_queue_current_size();
-    int get_front_order_id();
-    __restrict const T* buffer() noexcept;
-    void destroy_mapping() {
-        size_t size_required = sizeof(LockFreeSPSCQueue<T, capacity>);
-        this->~LockFreeSPSCQueue<T, capacity>();
-        munmap(this, size_required);
-    }
-
-    private:
-    Metadata_Producer mProducer;
-    Metadata_Consumer mConsumer;
-};
-
-template<typename T, size_t capacity>
 size_t LockFreeSPSCQueue<T, capacity>::load_head_acquire() noexcept {
-    mConsumer.head.load(std::memory_order_acquire);
-    return mConsumer.head;
+  mConsumer.head.load(std::memory_order_acquire);
+  return mConsumer.head;
 }
 
-template<typename T, size_t capacity>
+template <typename T, size_t capacity>
 size_t LockFreeSPSCQueue<T, capacity>::load_tail_acquire() noexcept {
-    mProducer.tail.load(std::memory_order_acquire);
-    return mProducer.tail;
+  mProducer.tail.load(std::memory_order_acquire);
+  return mProducer.tail;
 }
 
-template<typename T, size_t capacity>
-void LockFreeSPSCQueue<T, capacity>::producer_uncommitted_push(QueueOrder& qOrder, size_t current_local_tail) noexcept {
-    char* buffer_base = reinterpret_cast<char*>(this) + 128;
-    size_t slot_index = current_local_tail & (capacity - 1);
-    size_t byte_offset = slot_index << 6;
-    char* slot_address = buffer_base + byte_offset;
-    QueueOrder* casted_buffer_address = reinterpret_cast<QueueOrder*>(slot_address);
-    new (casted_buffer_address) QueueOrder(qOrder);
+template <typename T, size_t capacity>
+void LockFreeSPSCQueue<T, capacity>::producer_uncommitted_push(
+    QueueOrder &qOrder, size_t current_local_tail) noexcept {
+  char *buffer_base = reinterpret_cast<char *>(this) + 128;
+  size_t slot_index = current_local_tail & (capacity - 1);
+  size_t byte_offset = slot_index << 6;
+  char *slot_address = buffer_base + byte_offset;
+  QueueOrder *casted_buffer_address =
+      reinterpret_cast<QueueOrder *>(slot_address);
+  new (casted_buffer_address) QueueOrder(qOrder);
+  return;
+}
+
+template <typename T, size_t capacity>
+const QueueOrder *LockFreeSPSCQueue<T, capacity>::consumer_uncommitted_peek(
+    size_t consumer_local_head) noexcept {
+  const char *buffer_base = reinterpret_cast<const char *>(this);
+  size_t index = consumer_local_head & (capacity - 1);
+  size_t byte_offset = 128 + (index << 6);
+  const QueueOrder *slot =
+      reinterpret_cast<const QueueOrder *>(buffer_base + byte_offset);
+  return slot;
+}
+
+template <typename T, size_t capacity>
+void LockFreeSPSCQueue<T, capacity>::publish_tail_release(
+    size_t new_local_tail) {
+  mProducer.tail.store(new_local_tail, std::memory_order_release);
+  return;
+}
+
+template <typename T, size_t capacity>
+void LockFreeSPSCQueue<T, capacity>::publish_head_release(
+    size_t new_local_head) {
+  mConsumer.head.store(new_local_head, std::memory_order_release);
+  return;
+}
+
+template <typename T, size_t capacity>
+[[nodiscard]] __restrict const T *
+LockFreeSPSCQueue<T, capacity>::buffer() noexcept {
+  return reinterpret_cast<T *>(reinterpret_cast<char *>(this) + 2 * 64);
+}
+
+template <typename T, size_t capacity>
+LockFreeSPSCQueue<T, capacity>::LockFreeSPSCQueue()
+    : mProducer{0, 0}, mConsumer{0, 0} {
+  assert(sizeof(Metadata_Producer) == 64 &&
+         "Size of metadata producer should be 64 bytes \n");
+  assert(sizeof(Metadata_Consumer) == 64 &&
+         "Size of metadata consumer should be 64 bytes \n");
+  assert(reinterpret_cast<uintptr_t>(this) % 64 == 0 &&
+         "Assertion failed: Non functional core.. \n");
+  assert(mProducer.mask == mConsumer.mask &&
+         "Mask in both producer and consumer threads should be equal \n");
+  assert(sizeof(QueueOrder) % 64 == 0 &&
+         "QueueOrders should be 64 bytes in size \n");
+}
+
+// static factory method for class is the buffer allocation
+template <typename T, size_t capacity>
+LockFreeSPSCQueue<T, capacity> *LockFreeSPSCQueue<T, capacity>::create() {
+  const std::string required_proc_file_path = "/proc/sys/vm/nr_hugepages";
+  std::ifstream proc_file(required_proc_file_path);
+  if (!proc_file.is_open()) {
+    std::cerr << "Could not open file from path: " << required_proc_file_path
+              << " \n";
+    std::exit(EXIT_FAILURE);
+  }
+  int current_huge_pages = proc_file.get() - '0';
+  assert(current_huge_pages == 1 &&
+         "Assertion failed: Current huge pages value is not 1, allocation "
+         "unsuccessful \n");
+  if (current_huge_pages != 1) {
+    std::cerr << "Huge pages still not 1. Terminating the program \n";
+    std::exit(EXIT_FAILURE);
+  }
+  proc_file.close();
+  const size_t boundary = 2 * 1024 * 1024;
+  size_t base_size_of_class = sizeof(LockFreeSPSCQueue<T, capacity>);
+  size_t rounded_size = (base_size_of_class + (boundary - 1)) & ~(boundary - 1);
+  auto *mem_ptr =
+      mmap(NULL, rounded_size, PROT_READ | PROT_WRITE,
+           MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_POPULATE, -1, 0);
+  if (mem_ptr == MAP_FAILED) {
+    perror("mmap");
+    std::exit(EXIT_FAILURE);
+  }
+  unsigned long nodemask = 1;
+  unsigned long maxnode = 2;
+  int mbind_policy_result =
+      mbind(mem_ptr, boundary, MPOL_BIND, &nodemask, maxnode, MPOL_MF_STRICT);
+  if (mbind_policy_result == -1) {
+    perror("mbind");
+    std::exit(EXIT_FAILURE);
+  }
+  LockFreeSPSCQueue *obj = new (mem_ptr) LockFreeSPSCQueue();
+  char *offset_ptr = (char *)obj + 128;
+  size_t L1_stride = 2048;
+  assert(((uintptr_t)offset_ptr % L1_stride) != 0 &&
+         "Buffer offset should not be a multiple of L1 critical stride \n");
+  __m128i *simd_obj_ptr = reinterpret_cast<__m128i *>(mem_ptr);
+  __m128i zero_vector = _mm_setzero_si128();
+  static_cast<char *>(mem_ptr)[0] = 0;
+
+  for (size_t i = 0; i < (2 * 1024 * 1024) / 16; ++i) {
+    _mm_stream_si128(simd_obj_ptr + i, zero_vector);
+  }
+  return obj;
+}
+
+template <typename T, size_t capacity>
+void LockFreeSPSCQueue<T, capacity>::destroy(LockFreeSPSCQueue *ptr) {
+  if (!ptr) {
     return;
+  }
+  ptr->~LockFreeSPSCQueue();
 }
 
-template<typename T, size_t capacity>
-const QueueOrder* LockFreeSPSCQueue<T, capacity>::consumer_uncommitted_peek(size_t consumer_local_head) noexcept {
-    const char* buffer_base = reinterpret_cast<const char*>(this);
-    size_t index = consumer_local_head & (capacity - 1);
-    size_t byte_offset = 128 + (index << 6);
-    const QueueOrder* slot = reinterpret_cast<const QueueOrder*>(buffer_base + byte_offset);
-    return slot;
+template <typename T, size_t capacity>
+inline void
+LockFreeSPSCQueue<T, capacity>::commit_pop_order_from_queue(const T *ptr,
+                                                            size_t index) {
+  constexpr bool is_non_pod =
+      !(std::is_trivial_v<T> && std::is_standard_layout_v<T>);
+  if (is_non_pod) {
+    ptr->~T();
+  }
+  mConsumer.head.store((index + 1) & (capacity - 1), std::memory_order_release);
+  return;
 }
 
-template<typename T, size_t capacity>
-void LockFreeSPSCQueue<T, capacity>::publish_tail_release(size_t new_local_tail) {
-    mProducer.tail.store(new_local_tail, std::memory_order_release);
-    return;
-}
-
-template<typename T, size_t capacity>
-void LockFreeSPSCQueue<T, capacity>::publish_head_release(size_t new_local_head) {
-    mConsumer.head.store(new_local_head, std::memory_order_release);
-    return;
-}
-
-template<typename T, size_t capacity>
-[[nodiscard]] __restrict const T* LockFreeSPSCQueue<T, capacity>::buffer() noexcept {
-    return reinterpret_cast<T*>(reinterpret_cast<char*>(this) + 2 * 64);
-}
-
-template<typename T, size_t capacity>
-LockFreeSPSCQueue<T, capacity>::LockFreeSPSCQueue() : mProducer{0, 0}, mConsumer{0, 0} {
-    assert(sizeof(Metadata_Producer) == 64 && "Size of metadata producer should be 64 bytes \n");
-    assert(sizeof(Metadata_Consumer) == 64 && "Size of metadata consumer should be 64 bytes \n");
-    assert(reinterpret_cast<uintptr_t>(this)%64 == 0 && "Assertion failed: Non functional core.. \n");
-    assert(mProducer.mask == mConsumer.mask && "Mask in both producer and consumer threads should be equal \n");
-    assert(sizeof(QueueOrder) % 64 == 0 && "QueueOrders should be 64 bytes in size \n");
-}
-
-//static factory method for class is the buffer allocation
-template<typename T, size_t capacity>
-LockFreeSPSCQueue<T, capacity>* LockFreeSPSCQueue<T, capacity>::create() {
-    const std::string required_proc_file_path = "/proc/sys/vm/nr_hugepages";
-    std::ifstream proc_file(required_proc_file_path);
-    if (!proc_file.is_open()) {
-        std::cerr << "Could not open file from path: " << required_proc_file_path << " \n";
-        std::exit(EXIT_FAILURE);
-    }
-    int current_huge_pages = proc_file.get() - '0';
-    assert(current_huge_pages == 1 && "Assertion failed: Current huge pages value is not 1, allocation unsuccessful \n");
-    if (current_huge_pages != 1) {
-        std::cerr << "Huge pages still not 1. Terminating the program \n";
-        std::exit(EXIT_FAILURE);
-    }
-    proc_file.close();
-    const size_t boundary = 2 * 1024 * 1024;
-    size_t base_size_of_class = sizeof(LockFreeSPSCQueue<T, capacity>);
-    size_t rounded_size = (base_size_of_class + (boundary - 1)) & ~(boundary - 1);
-    auto* mem_ptr = mmap(NULL, rounded_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_POPULATE, -1, 0);
-    if (mem_ptr == MAP_FAILED) {
-        perror("mmap");
-        std::exit(EXIT_FAILURE);
-    }
-    unsigned long nodemask = 1;
-    unsigned long maxnode = 2;
-    int mbind_policy_result = mbind(mem_ptr, boundary, MPOL_BIND, &nodemask, maxnode, MPOL_MF_STRICT);
-    if (mbind_policy_result == -1) {
-        perror("mbind");
-        std::exit(EXIT_FAILURE);
-    }
-    LockFreeSPSCQueue* obj = new (mem_ptr) LockFreeSPSCQueue();
-    char* offset_ptr = (char*)obj + 128;
-    size_t L1_stride = 2048;
-    assert(((uintptr_t)offset_ptr % L1_stride) != 0 && "Buffer offset should not be a multiple of L1 critical stride \n");
-    __m128i* simd_obj_ptr = reinterpret_cast<__m128i*>(mem_ptr);
-    __m128i zero_vector = _mm_setzero_si128();
-    static_cast<char*>(mem_ptr)[0] = 0;
-
-    for(size_t i = 0; i < (2*1024*1024) / 16; ++i) {
-        _mm_stream_si128(simd_obj_ptr + i, zero_vector);
-    }
-    return obj; 
-}
-
-template<typename T, size_t capacity>
-void LockFreeSPSCQueue<T, capacity>::destroy(LockFreeSPSCQueue* ptr) {
-    if (!ptr) { return; }
-    ptr->~LockFreeSPSCQueue();
-}
-
-
-template<typename T, size_t capacity>
-inline void LockFreeSPSCQueue<T, capacity>::commit_pop_order_from_queue(const T* ptr, size_t index) {
-    constexpr bool is_non_pod = !(std::is_trivial_v<T> && std::is_standard_layout_v<T>);
-    if (is_non_pod) { ptr->~T(); }
-    mConsumer.head.store((index + 1)&(capacity-1), std::memory_order_release);
-    return;
-}
-
-template<typename T, size_t capacity>
+template <typename T, size_t capacity>
 int LockFreeSPSCQueue<T, capacity>::get_front_order_id() {
-    if (queue_empty()) {
-        return -1;
-    }
-    auto* buf = buffer();
-    return buf[mConsumer.head & (capacity - 1)].order_id;
+  if (queue_empty()) {
+    return -1;
+  }
+  auto *buf = buffer();
+  return buf[mConsumer.head & (capacity - 1)].order_id;
 }
 
-template<typename T, size_t capacity>
+template <typename T, size_t capacity>
 bool LockFreeSPSCQueue<T, capacity>::queue_full() {
-    size_t head = mConsumer.head.load(std::memory_order_acquire);
-    size_t tail = mProducer.tail.load(std::memory_order_acquire);
-    return ((tail + 1) & (capacity - 1)) == head;
+  size_t head = mConsumer.head.load(std::memory_order_acquire);
+  size_t tail = mProducer.tail.load(std::memory_order_acquire);
+  return ((tail + 1) & (capacity - 1)) == head;
 }
 
-template<typename T, size_t capacity>
+template <typename T, size_t capacity>
 bool LockFreeSPSCQueue<T, capacity>::queue_empty() {
-    size_t head = mConsumer.head.load(std::memory_order_seq_cst);
-    size_t tail = mProducer.tail.load(std::memory_order_seq_cst);
-    return head == tail;
+  size_t head = mConsumer.head.load(std::memory_order_seq_cst);
+  size_t tail = mProducer.tail.load(std::memory_order_seq_cst);
+  return head == tail;
 }
 
-template<typename T, size_t capacity>
+template <typename T, size_t capacity>
 int LockFreeSPSCQueue<T, capacity>::get_queue_current_size() {
-    size_t head = mConsumer.head.load(std::memory_order_acquire);
-    size_t tail = mProducer.tail.load(std::memory_order_acquire);
-    return (tail - head) & (capacity - 1);
+  size_t head = mConsumer.head.load(std::memory_order_acquire);
+  size_t tail = mProducer.tail.load(std::memory_order_acquire);
+  return (tail - head) & (capacity - 1);
 }
--- include/vulcan/queue_core.hpp
+++ include/vulcan/queue_core.hpp
@@ -1,106 +1,124 @@
 #include <array>
+#include <cassert>
+#include <cerrno>
 #include <cstddef>
 #include <cstdint>
 #include <emmintrin.h>
-#include <pthread.h>
-#include <cerrno>
 #include <iostream>
-#include <cassert>
+#include <pthread.h>
 
 const size_t TOTAL_WARMUP_OPS = 1000000;
 
 void pinThreadToCore(pthread_t thread_id, int core_id) {
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(core_id, &cpuset);
+  cpu_set_t cpuset;
+  CPU_ZERO(&cpuset);
+  CPU_SET(core_id, &cpuset);
 
-    int set_affinity_mask_result = pthread_setaffinity_np(thread_id, sizeof(cpu_set_t), &cpuset);
-    if (set_affinity_mask_result != 0) {
-        std::cerr << "Error calling pthread_setaffinity_np for core: " << core_id << "has error: " << EXIT_FAILURE << " \n";
-    }
-    // Ensuring the thread is allowed to run on the target core
-    bool is_set = CPU_ISSET(core_id, &cpuset);
-    // Ensuring thread is ONLY pinned to this core (affinity count == 1)
-    int total_cores_in_mask = CPU_COUNT(&cpuset);
-    assert(is_set && total_cores_in_mask == 1 && "Thread is not strictly pinned to the target core!");
+  int set_affinity_mask_result =
+      pthread_setaffinity_np(thread_id, sizeof(cpu_set_t), &cpuset);
+  if (set_affinity_mask_result != 0) {
+    std::cerr << "Error calling pthread_setaffinity_np for core: " << core_id
+              << "has error: " << EXIT_FAILURE << " \n";
+  }
+  // Ensuring the thread is allowed to run on the target core
+  bool is_set = CPU_ISSET(core_id, &cpuset);
+  // Ensuring thread is ONLY pinned to this core (affinity count == 1)
+  int total_cores_in_mask = CPU_COUNT(&cpuset);
+  assert(is_set && total_cores_in_mask == 1 &&
+         "Thread is not strictly pinned to the target core!");
 }
 
-bool populate_stack_buffer(std::array<QueueOrder, 8>& my_queueOrder) {
-    for (size_t i = 0; i < 8; ++i) {
-        my_queueOrder[i] = {i, (i+1)*1.33, i+100};
-    }
-    assert(my_queueOrder[7].order_id == 7 && "Assertion failed: Stack buffer not populated correctly \n");
-    return true;
+bool populate_stack_buffer(std::array<QueueOrder, 8> &my_queueOrder) {
+  for (size_t i = 0; i < 8; ++i) {
+    my_queueOrder[i] = {i, (i + 1) * 1.33, i + 100};
+  }
+  assert(my_queueOrder[7].order_id == 7 &&
+         "Assertion failed: Stack buffer not populated correctly \n");
+  return true;
 }
 
-void warm_up_producer(LockFreeSPSCQueue<QueueOrder, 256>& lock_free_queue_ref, std::array<QueueOrder, 8>& queue_orders_ref) {
-    size_t local_current_tail = 0;
-    size_t local_current_head_cached = 0;
-    populate_stack_buffer(queue_orders_ref);
-    uint64_t batch_count = 0;
-    for (size_t i = 0; i < TOTAL_WARMUP_OPS; ++i) {
-        size_t next_local_tail = (local_current_tail + 1) & 255;
-        if (next_local_tail == local_current_head_cached) {
-            while (next_local_tail == local_current_head_cached) {
-                _mm_pause();
-                local_current_head_cached = lock_free_queue_ref.load_head_acquire();
-            }
-        }
-        size_t buffer_index = local_current_tail & 7;
-        lock_free_queue_ref.producer_uncommitted_push(queue_orders_ref[buffer_index], local_current_tail);
-        local_current_tail = next_local_tail;
-        batch_count ++;
-        if ((batch_count & 7) == 0) {
-            lock_free_queue_ref.publish_tail_release(local_current_tail);
-        } 
+void warm_up_producer(LockFreeSPSCQueue<QueueOrder, 256> &lock_free_queue_ref,
+                      std::array<QueueOrder, 8> &queue_orders_ref) {
+  size_t local_current_tail = 0;
+  size_t local_current_head_cached = 0;
+  populate_stack_buffer(queue_orders_ref);
+  uint64_t batch_count = 0;
+  for (size_t i = 0; i < TOTAL_WARMUP_OPS; ++i) {
+    size_t next_local_tail = (local_current_tail + 1) & 255;
+    if (next_local_tail == local_current_head_cached) {
+      while (next_local_tail == local_current_head_cached) {
+        _mm_pause();
+        local_current_head_cached = lock_free_queue_ref.load_head_acquire();
+      }
     }
-    lock_free_queue_ref.publish_tail_release(local_current_tail);
-    return;
+    size_t buffer_index = local_current_tail & 7;
+    lock_free_queue_ref.producer_uncommitted_push(
+        queue_orders_ref[buffer_index], local_current_tail);
+    local_current_tail = next_local_tail;
+    batch_count++;
+    if ((batch_count & 7) == 0) {
+      lock_free_queue_ref.publish_tail_release(local_current_tail);
+    }
+  }
+  lock_free_queue_ref.publish_tail_release(local_current_tail);
+  return;
 }
 
-void warm_up_consumer(LockFreeSPSCQueue<QueueOrder, 256>& lock_free_queue_ref) {
-    size_t local_head_idx = 0;
-    uint64_t local_tail_cached = 0;
-    uint64_t batch_count = 0;
-    double local_register_accumulator_0, local_register_accumulator_1, local_register_accumulator_2, local_register_accumulator_3, \
-    local_register_accumulator_4, local_register_accumulator_5, local_register_accumulator_6, local_register_accumulator_7 = 0.0;
-    
-    for (size_t i = 0; i < TOTAL_WARMUP_OPS; ++i) {
-        if (local_head_idx == local_tail_cached) {
-            while (local_head_idx == local_tail_cached) {
-                _mm_pause();
-                local_tail_cached = lock_free_queue_ref.load_tail_acquire();
-            }
-        }
-        
-        const QueueOrder* current_head_index_ptr_0 = lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx);
-        const QueueOrder* current_head_index_ptr_1 = lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 1);
-        const QueueOrder* current_head_index_ptr_2 = lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 2);
-        const QueueOrder* current_head_index_ptr_3 = lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 3);
-        const QueueOrder* current_head_index_ptr_4 = lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 4);
-        const QueueOrder* current_head_index_ptr_5 = lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 5);
-        const QueueOrder* current_head_index_ptr_6 = lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 6);
-        const QueueOrder* current_head_index_ptr_7 = lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 7);
-        
-        
-        local_register_accumulator_0 += current_head_index_ptr_0->price;
-        local_register_accumulator_1 += current_head_index_ptr_1->price;
-        local_register_accumulator_2 += current_head_index_ptr_2->price;
-        local_register_accumulator_3 += current_head_index_ptr_3->price;
-        local_register_accumulator_4 += current_head_index_ptr_4->price;
-        local_register_accumulator_5 += current_head_index_ptr_5->price;
-        local_register_accumulator_6 += current_head_index_ptr_6->price;
-        local_register_accumulator_7 += current_head_index_ptr_7->price;        
-        
-        local_head_idx = (local_head_idx + 8) & 255;
-        batch_count ++;
-        if ((batch_count & 7) == 0) {
-            lock_free_queue_ref.publish_head_release(local_head_idx);
-        }
+void warm_up_consumer(LockFreeSPSCQueue<QueueOrder, 256> &lock_free_queue_ref) {
+  size_t local_head_idx = 0;
+  uint64_t local_tail_cached = 0;
+  uint64_t batch_count = 0;
+  double local_register_accumulator_0, local_register_accumulator_1,
+      local_register_accumulator_2, local_register_accumulator_3,
+      local_register_accumulator_4, local_register_accumulator_5,
+      local_register_accumulator_6, local_register_accumulator_7 = 0.0;
+
+  for (size_t i = 0; i < TOTAL_WARMUP_OPS; ++i) {
+    if (local_head_idx == local_tail_cached) {
+      while (local_head_idx == local_tail_cached) {
+        _mm_pause();
+        local_tail_cached = lock_free_queue_ref.load_tail_acquire();
+      }
     }
-    lock_free_queue_ref.publish_head_release(local_head_idx);
-    auto total_accumulator_sum = ((local_register_accumulator_0 + local_register_accumulator_1) + (local_register_accumulator_2 + local_register_accumulator_3) + \
-        (local_register_accumulator_4 + local_register_accumulator_5) + (local_register_accumulator_6 + local_register_accumulator_7));
-    
-    return;
+
+    const QueueOrder *current_head_index_ptr_0 =
+        lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx);
+    const QueueOrder *current_head_index_ptr_1 =
+        lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 1);
+    const QueueOrder *current_head_index_ptr_2 =
+        lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 2);
+    const QueueOrder *current_head_index_ptr_3 =
+        lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 3);
+    const QueueOrder *current_head_index_ptr_4 =
+        lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 4);
+    const QueueOrder *current_head_index_ptr_5 =
+        lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 5);
+    const QueueOrder *current_head_index_ptr_6 =
+        lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 6);
+    const QueueOrder *current_head_index_ptr_7 =
+        lock_free_queue_ref.consumer_uncommitted_peek(local_head_idx + 7);
+
+    local_register_accumulator_0 += current_head_index_ptr_0->price;
+    local_register_accumulator_1 += current_head_index_ptr_1->price;
+    local_register_accumulator_2 += current_head_index_ptr_2->price;
+    local_register_accumulator_3 += current_head_index_ptr_3->price;
+    local_register_accumulator_4 += current_head_index_ptr_4->price;
+    local_register_accumulator_5 += current_head_index_ptr_5->price;
+    local_register_accumulator_6 += current_head_index_ptr_6->price;
+    local_register_accumulator_7 += current_head_index_ptr_7->price;
+
+    local_head_idx = (local_head_idx + 8) & 255;
+    batch_count++;
+    if ((batch_count & 7) == 0) {
+      lock_free_queue_ref.publish_head_release(local_head_idx);
+    }
+  }
+  lock_free_queue_ref.publish_head_release(local_head_idx);
+  auto total_accumulator_sum =
+      ((local_register_accumulator_0 + local_register_accumulator_1) +
+       (local_register_accumulator_2 + local_register_accumulator_3) +
+       (local_register_accumulator_4 + local_register_accumulator_5) +
+       (local_register_accumulator_6 + local_register_accumulator_7));
+
+  return;
 }
